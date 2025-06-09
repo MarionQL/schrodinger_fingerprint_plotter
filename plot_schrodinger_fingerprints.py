3f551f067ca6e10f96c0ff68a9fdbc9f90ca38c6
Marion Q LoPresti <Marion.lopresti@gmail.com>
2025-06-09 13:47:02 -0400

    Update plot_schrodinger_fingerprints.py

--- plot_schrodinger_fingerprints.py
+++ plot_schrodinger_fingerprints.py
@@ -12,7 +12,7 @@ def load_data(file_path):
     '''
     arg: file_path (str) is path to CSV from user input
     returns: pandas dataframe of the CSV'''
-    return pd.read_csv(file_path)
+    return pd.read_csv(file_path, dtype=str)
 
 def extract_ligand(df):
     '''extracts ligand from the first column, can handle these ligand formats in the csv:
